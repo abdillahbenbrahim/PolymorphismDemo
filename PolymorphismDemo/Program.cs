4baaeb44a2f0a1a3f94b3ab09f4e27115cad2f82
abdillahbenbrahim <abenbrahim76@gmail.com>
2017-03-06 19:35:17 -0500

    Demo

---
+++ PolymorphismDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismDemo
+{
+    class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Performing a class drawing tasks");
+        }
+    }
+}
